d05fd1c8aff92f389dad73f3b9018f558476b341
shravanmeena <shravanmeena47@gmail.com>
2022-07-12 11:24:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { loadScript } from "./helper/loadScript";
 
 function App() {
+  const [token, setToken] = useState("");
+
+  const onScriptLoad = async () => {
+    const res = await loadScript(
+      "https://securegw.paytm.in/merchantpgpui/checkoutjs/merchants/ZHMpwy77672646700865.js"
+    );
+
+    if (!res) {
+      alert("SDK failed to load. Are you online?");
+      return;
+    }
+
+    if (res) {
+      console.log("script loaded!", process.env.REACT_APP_PAYMENT_MODE);
+
+      var easebuzzCheckout = new window.EasebuzzCheckout(
+        process.env.REACT_APP_EASEBUZZ_KEY, // key
+        process.env.REACT_APP_EASEBUZZ_ENV //test||prod
+      );
+      var options = {
+        access_key: token, // access key received via Initiate Payment
+        onResponse: (response) => {
+          console.log(response, "response");
+
+          if (response.status === "userCancelled") {
+          }
+
+          if (response.status === "success") {
+          }
+          console.log(response, "response");
+        },
+        theme: "#123456", // color hex
+      };
+      easebuzzCheckout.initiatePayment(options);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+
+      <input
+        onChange={(e) => setToken(e.target.value)}
+        type="text"
+        placeholder="Enter Your Token"
+      />
+      <br />
+
+      {/* <button onClick={onScriptLoad}>TEST PAYTM</button> */}
+
+      <br />
+      <br />
+      <br />
+
+      <button onClick={onScriptLoad}>TEST EASEBUZZ</button>
     </div>
   );
 }
